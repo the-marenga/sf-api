6e88a556abd4bc1aa56a87447d479b52b4e700d9
Marenga <marengamatdrb@gmail.com>
2024-11-11 15:18:06 +0100

    Fixes and stuff

--- src/gamestate/items.rs
+++ src/gamestate/items.rs
@@ -457,10 +457,16 @@ impl GemSlot {
             warn!("Invalid gem power {gem_pwr}");
             return None;
         };
-        match GemType::parse(slot_val, value) {
+        let res = match GemType::parse(slot_val, value) {
             Some(typ) => Some(GemSlot::Filled(Gem { typ, value })),
             None => Some(GemSlot::Empty),
-        }
+        };
+
+        if value == 318 {
+            warn!("Was {slot_val}");
+        };
+
+        res
     }
 }
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
